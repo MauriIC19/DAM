8eac75d0012fa122a153c79bcc7c80bacf632278
Mauricio IC <mauricio_ic19@outlook.com>
2024-03-05 14:26:37 -0600

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, TextInput, Text } from "react-native";
 
 export default function App() {
+
+  const [name, setName] = useState("Mundo");
+  //let name = "Mundo";
+
+  const inputManager = (text) => {
+    setName(text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.header}>
+        Hola, {name}.
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={inputManager}
+        placeholder="useless placeholder"
+        secureTextEntry={false}
+      />
+      <StatusBar style="light" />
     </View>
   );
 }
@@ -13,8 +30,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  input: {
+    width: "90%",
+    height: 60,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "white"
+  },
+  header: {
+    color: "white",
+    fontSize: 80,
+    fontFamily: 'Helvetica'
+  }
 });
